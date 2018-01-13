8fad04389f9382cfb6b96c80df1fabd18cf11519
Amaresh <amareshbingumalla@gmail.com>
2018-01-13 00:40:07 -0700

    Added changes to read values from env file

--- minSeats.py
+++ minSeats.py
@@ -2,10 +2,18 @@ import time
 from selenium.webdriver.support.ui import Select # Install selenium packages using pip on pycharm
 from selenium import webdriver
 from twilio.rest import Client
+from dotenv import load_dotenv
+import os
 
-account_sid = "" #your sid
-auth_token = "" # your token
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+dotenv_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path)
 
+
+account_sid = os.getenv('ACCOUNT_SID') #your sid
+auth_token = os.getenv('AUTH_TOKEN') # your token
+from_num = os.getenv('FROM_NUMBER')
+to_num = os.getenv('TO_NUMBER')
 client = Client(account_sid, auth_token)
 
 
@@ -33,7 +41,7 @@ if(driver.find_elements_by_css_selector('.col-xs-3') and len(driver.find_element
     if(int(numSeats) > 0):
         print("Yaay" + numSeats + " for " + subject + classNum) 
         #change numbers
-        client.api.account.messages.create(to="+19999999999",from_="+19999999999",body="Just " + numSeats +" seats left for " + subject + " "+classNum)
+        client.api.account.messages.create(to=to_num, from_=from_num, body="Just " + numSeats +" seats left for " + subject + " "+classNum)
     else:
         print("Uh no.")
 
